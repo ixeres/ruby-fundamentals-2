bfc225d201580df4a31b89984bd6f84b8df27fbd
Devin Leduc <ixeres@gmail.com>
2017-03-28 14:38:58 -0400

    Initial commit with exercise 5 and exercise not 5.

---
+++ exercise-not5.rb
@@ -0,0 +1,3 @@
+def greet_backwards(name)
+  p "Hello, #{name.reverse * 2}! Your name is backwards!"
+end
---
+++ exercise5.rb
@@ -0,0 +1,10 @@
+#converting Fahrenheit to Celsius with a method.
+
+def convert_fahrenheit #defines the method and the value being passed in.
+  num = gets.to_i
+  num - 32 * 5/9
+end
+
+puts "Give me a temperature in Fahrenheit, jabroni!" #asks for user input
+  gets.chomp
+  puts "That is #{convert_fahrenheit} in Celsius! Get educated!" #method called in output string.
