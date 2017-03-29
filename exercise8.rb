2c5c93bdf2753b90ff6c62ba82d34ebee7cbc582
Devin Leduc <ixeres@gmail.com>
2017-03-29 05:32:46 -0400

    Exercise 8 complete. ONWARDS.

--- exercise8.rb
+++ exercise8.rb
@@ -1,9 +1,10 @@
 #Calculate expenses using an array! Okay!
 
-expenses = [250, 7.95, 30.95, 16.50]
-expenses.sum
-end
+expenses_array = [250, 7.95, 30.95, 16.50]
+# sums the array = expenses_array.sum
 
-def total([])
-  expenses = [].sum
-  put "Total expenses = #{expenses}"
+def total_expense(expenses)
+  expenses.sum
+  total = expenses.sum
+    return total
+end
