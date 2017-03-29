8427bd07a4ae52b55e4b7c235da0c1f7ef6f457f
Devin Leduc <ixeres@gmail.com>
2017-03-29 10:34:58 -0400

    Accidentally did the second part of exercise 11 without knowing it. Now it is done.

--- exercise11.rb
+++ exercise11.rb
@@ -4,10 +4,16 @@ my_dogs = [
   {:name => 'Midnight', :position => 11},
 ]
 
+their_dogs = [
+  {:name => 'Puglie', :position => 7}
+  {:name => 'Doge', :position => 12}
+  {:name => 'Puggo', :position => 27} #PUGGO GO FAR.
+]
+
 def get_absent_dogs(dogs, boundary = 10) #puts my_dogs into 'dogs', sets default boundary to 10
   dogs.each do |dog| #takes my_dogs array, iterates them
     if (dog[:position]) > boundary #takes iteration and checks value against default boundary
-    puts "#{dog} is out of the yard!" #tells which dog/value pair is out of the yard.
+    puts "#{dog[:name]} is out of the yard! Get back here, #{dog[:name]}!" #tells which dog/value pair is out of the yard.
   end
 end
 end
